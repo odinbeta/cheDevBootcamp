0c67f64b248265dd2439301b4075f6db7f41b222
odinbeta <mvlpeter@gmail.com>
2019-08-20 04:47:29 +0000

    server running

--- myFirstExpressApp/app.js
+++ myFirstExpressApp/app.js
@@ -1,3 +1,13 @@
 var express = require("express");
 //la primera importa la libreria, la segunda es para usarla
 var app = express();
+
+app.get("/", function(req,res){
+    res.send("Hello There!");
+});
+
+app.listen(3000, function(){
+   console.log("server is running") 
+    
+});
+
